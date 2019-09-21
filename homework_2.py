a15556cfb151c99df67eaf1b8388b076c0681461
artmukr <artmukr@gmail.com>
2019-09-21 14:15:11 +0300

    my homework_2 was done

---
+++ homework_2.py
@@ -0,0 +1,17 @@
+name = 'Artem'
+current_day = 'Saturday'
+print('It`s a good ' + current_day + ', ' + name + '!')
+
+first_name = 'Artem'
+last_name = 'Syrota'
+print('I wish you a lot of money, ' + first_name + ' ' + last_name)
+
+not_a = 58
+not_b = 6
+
+print(
+    not_a + not_b, '|', not_a - not_b, '|',
+    not_a / not_b, '|', not_a * not_b, '|',
+    not_a ** not_b, '|', not_a % not_b, '|',
+    not_a // not_b, '|'
+)
